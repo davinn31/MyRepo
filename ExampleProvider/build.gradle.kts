16e4cc1652a58f65c6446e03aee9de63e82f0d19
Davin <m.davin3116@gmail.com>
2026-05-21 23:55:02 +0700

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -7,24 +7,17 @@ dependencies {
 version = 1
 
 cloudstream {
-    packageName = "com.davinn31" 
+    // SESUAIKAN DENGAN ISI FILE KOTLIN ANDA:
+    packageName = "com.example" 
     
     description = "Lorem ipsum"
     authors = listOf("Cloudburst", "diri")
     
-    /*
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    * */
-    status = 1 // Will be 3 if unspecified
+    status = 1
     tvTypes = listOf("Movie")
     requiresResources = true
     language = "en"
     
-    // Random CC logo I found
     iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
 }
 
